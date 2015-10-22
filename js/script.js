adde7c4e0a3988337a46e982aa7ccb43a71c7ca2
SERGIO ANTONIO <sergio.mantonio@gmail.com>
2015-10-22 01:32:36 -0700

    atualização ptrofile

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function() {
+
+  var links = $("#myNavbar a");
+  $(window).scroll(function() {
+    var topScroll = $(window).scrollTop();
+    links.each(function() {
+      var href       = $(this).attr('href');
+      var el         = $(href);
+      var posSection = el.offset().top;
+      var hSection   = el.height();
+
+      if(posSection <= topScroll && (posSection + hSection) > topScroll){
+        links.removeClass("active");
+        $(this).addClass("active");        
+      }
+      else 
+      {
+        $(this).removeClass("active");
+      }
+    })
+
+  })
+});
+/*
+$(document).ready(function() {
+
+  $(".banner").css({"height": $(window).height() + "px"});  
+
+  $(window).scroll(function() {
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll > $(window).height())
+    {
+      $(".my-info-nav").css({"display": "block", "margin-left": "0px"});
+
+    }
+    else
+    {
+      $(".my-info-nav").css({"display":"none"});
+    }
+
+  });
+
+});*/
